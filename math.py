a3debdca5ca6ff4e0c639c3c9f7704b3c03bbdf7
GauravRaj <gaurav_raj1@persistent.com>
2022-01-03 10:31:40 +0530

    impl add fn

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
 #add implementation
 def add(x,y):
-    return x+y
+   return x+y
 #subtract impleamenttaion
 def subtract(x,y):
-    return x-y      #on master branch
+   pass
 #multiply implemnentaion
 def multiply(x,y):
-    return x*y          #on bug456 branch
+  pass
 #divide implementation
 def divide(x,y):
-   
+   pass
